200b341892b49617fa7da35fbae9e2603e86e990
Miron <popovmiron@gmail.com>
2024-12-19 16:26:56 +0700

    Цвета для барабана

---
+++ app/src/main/java/ru/aston/polechudes/DrumColors.kt
@@ -0,0 +1,15 @@
+package ru.aston.polechudes
+
+import android.graphics.Color
+
+object DrumColors {
+    val colors = listOf(
+        Color.RED,                                  //0
+        Color.rgb(255, 165, 0),     //1
+        Color.YELLOW,                               //2
+        Color.GREEN,                                //3
+        Color.CYAN,                                 //4 голубой
+        Color.BLUE,                                 //5
+        Color.MAGENTA                               //6 розовый
+    )
+}
